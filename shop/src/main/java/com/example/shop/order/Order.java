5f03ee78ae8be48e3785162e68f1b280717aba63
astrokan <astrokan@naver.com>
2025-11-05 17:16:01 +0900

    feat: week4 추가

---
+++ shop/src/main/java/com/example/shop/order/Order.java
@@ -0,0 +1,38 @@
+package com.example.shop.order;
+
+import java.time.LocalDateTime;
+
+import com.example.shop.member.Member;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "total_price")
+    private int totalPrice;
+
+    @Column(name = "point_used")
+    private int pointUsed;
+
+    @Column(name = "cash_amount")
+    private int cashAmount;
+
+    @Column(name = "status", length = 25)
+    private String status;
+}
